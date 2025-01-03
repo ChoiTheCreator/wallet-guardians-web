955b15f5c592288b39b2e681bb561f2a1f31d225
최원빈 <wonbin109@naver.com>
2025-01-03 19:48:42 +0900

    feat navbar hambuger toggling 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Routes, Route } from 'react-router-dom'; // BrowserRouter 제거
 import StartPage from './pages/StartPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignUpPage';
+import MainPage from './pages/MainPage';
 const App = () => {
   return (
     <Routes>
+      <Route path="/main" element={<MainPage />} />
       <Route path="/" element={<StartPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import '../style/MainPage.scss';
+import Navbar from '../components/Navbar';
 const MainPage = () => {
-  return <div></div>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="main-page">
+      {/* 햄버거 버튼 */}
+      <button
+        className={`hamburger-button ${isSidebarOpen ? 'shifted' : ''}`}
+        onClick={toggleSidebar}
+      >
+        ☰
+      </button>
+      <Navbar isSidebarOpen={isSidebarOpen}></Navbar>
+      {/* Main Content */}
+      <div className={`content ${isSidebarOpen ? 'shifted' : ''}`}>
+        <h1>Main Content Area</h1>
+        <p>여기에 메인 콘텐츠가 표시됩니다.</p>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
