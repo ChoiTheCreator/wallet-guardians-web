367ef060a6bb5425cbd49ec5e6469ed964604217
leesj0188 <lsj01881@gmail.com>
2025-01-22 17:48:38 +0900

    <fix>

--- src/pages/ReceiptPage.jsx
+++ src/pages/ReceiptPage.jsx
@@ -32,8 +32,8 @@ const ReceiptPage = () => {
     try {
       const response = await axios.post('http://clovaocr-api-kr.ncloud.com/external/v1/37918/8d26c27c4212d627768bd830b680507a90317c8b58e7c516440c36f734e42705', formData, {
         headers: {
-          'X-NCP-APIGW-API-KEY-ID': 'ncp_iam_BPAMKR4jfOcFGcClLn60',
-          'X-NCP-APIGW-API-KEY': 'ncp_iam_BPKMKRCNOP0fb9Qvs1WvlYOwpvJgE1Kmsy',
+          'X-NCP-APIGW-API-KEY-ID': '',
+          'X-NCP-APIGW-API-KEY': '',
           'Content-Type': 'multipart/form-data'
         }
       });
