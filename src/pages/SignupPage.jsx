34513c127d1ea98e97d69476b0c32db2a306f48f
최원빈 <wonbin109@naver.com>
2025-01-25 21:20:11 +0900

    <feat> 회원가입시 실패 및 성공에 따른 에러메시지, 성공메시지 출력

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -4,40 +4,49 @@ import { signup } from '../api/authApi.jsx';
 import '../style/SignupPage.scss';
 
 const SignupPage = ({ closeSignupModal }) => {
-  const [userName, setUserName] = useState('');
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
 
+  //성공 혹은 실패시 보여지는 모달 메시지 (객체로 초기화, 속성은 type, message)
+  const [modalMessage, setModalMessage] = useState({ type: '', message: '' });
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
-      setErrorMessage('비밀번호가 일치하지 않습니다.');
+      setModalMessage({
+        type: 'error',
+        message: '비밀번호가 일치하지 않습니다.',
+      });
       return;
     }
 
     if (password.length < 8) {
-      setErrorMessage('비밀번호는 8자 이상이어야 합니다.');
+      setModalMessage({
+        type: 'error',
+        message: '비밀번호는 8자 이상이어야합니다.',
+      });
       return;
     }
 
-    setErrorMessage('');
-
     try {
-      const data = await signup(userName, email, password); // 상대 경로를 사용해 API 호출
+      //매개변수
+      const data = await signup(username, email, password); // 상대 경로를 사용해 API 호출
       console.log('회원가입 성공:', data);
-      setSuccessMessage('회원가입이 완료되었습니다!');
+      //성공시에는 모달메시지를 성공했다고 표시한다. type -> success, message -> 성공 ㅋ
+      setModalMessage({ type: 'success', message: '회원가입에 성공했습니다!' });
 
-      setUserName('');
+      setUsername('');
       setEmail('');
       setPassword('');
       setConfirmPassword('');
     } catch (error) {
-      console.error(error);
-      setErrorMessage('회원가입에 실패했습니다.');
+      setModalMessage({
+        type: 'error',
+        message: '서버 오류로 회원가입에 실패했습니다.',
+      });
+      console.log(error);
     }
   };
 
@@ -54,8 +63,8 @@ const SignupPage = ({ closeSignupModal }) => {
             type="text"
             placeholder="이름"
             className="input-field"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
           <input
@@ -82,15 +91,18 @@ const SignupPage = ({ closeSignupModal }) => {
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
-          {errorMessage && <p className="error-message">{errorMessage}</p>}
-          {successMessage && (
-            <p className="success-message">{successMessage}</p>
-          )}
           <button type="submit" className="signup-button">
             회원가입
           </button>
+          {/* 모달 메시지 내의 메세지가 존재한다면 type으로 클래스 탈부착하여 메세지를 보여준다.*/}
+          {modalMessage.message && (
+            <div className={`modal-message ${modalMessage.type}`}>
+              {modalMessage.message}
+            </div>
+          )}
         </form>
       </div>
+      {}
     </div>
   );
 };
