8fe2a8647d6e519235e4bf85502c038361a1eac5
최원빈 <wonbin109@naver.com>
2025-01-25 21:44:59 +0900

    <Feat> LoginPage 모달메시지도 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,28 +1,27 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import '../style/LoginPage.scss';
 import SignupPage from './SignupPage';
 import { useNavigate } from 'react-router-dom';
 import { GoalContext } from '../context/GoalContext';
+
 import { login } from '../api/authApi.jsx'; // 로그인 API 호출
 
 const LoginPage = () => {
   const [isSignupOpen, setIsSignupOpen] = useState(false); // 회원가입 모달 상태
   const [email, setEmail] = useState(''); // 이메일 입력 값
   const [password, setPassword] = useState(''); // 비밀번호 입력 값
-  const [errorMessage, setErrorMessage] = useState(''); // 에러 메시지
 
+  const [modalMessage, setModalMessage] = useState({ type: '', message: '' });
   const navigate = useNavigate();
   const { goalAmount } = useContext(GoalContext); // 목표 금액 상태 (컨텍스트 사용)
 
-  // 회원가입 모달 열기
+  // 회원가입 모달 여닫이
   const openSignupModal = () => setIsSignupOpen(true);
-
-  // 회원가입 모달 닫기
   const closeSignupModal = () => setIsSignupOpen(false);
 
   // 로그인 처리 함수
   const handleLogin = async (e) => {
-    e.preventDefault(); // 기본 폼 제출 동작 방지
+    e.preventDefault();
 
     try {
       // API 요청 보내기
@@ -33,23 +32,38 @@ const LoginPage = () => {
       localStorage.setItem('token', token);
       localStorage.setItem('refreshToken', refreshToken);
 
-      setErrorMessage(''); // 에러 메시지 초기화
-
       // 목표 금액 설정 여부에 따라 페이지 이동
       if (goalAmount && goalAmount > 0) {
+        setModalMessage({
+          type: 'success',
+          message: '로그인 성공! 메인화면으로 다이렉트됩니다.',
+        });
         navigate('/main'); // 목표 금액이 설정된 경우
       } else {
+        setModalMessage({
+          type: 'success',
+          message: '로그인 성공! 시작화면으로 다이렉트됩니다.',
+        });
         navigate('/initial'); // 목표 금액이 설정되지 않은 경우
       }
     } catch (error) {
       console.error('로그인 실패:', error);
       // 에러 메시지 설정
-      setErrorMessage(
-        error.response?.data?.message ||
-          '아이디 혹은 비밀번호가 올바르지 않습니다.'
-      );
+      setModalMessage({
+        type: 'error',
+        message: '로그인 실패! 다시 시도해주세요.',
+      });
     }
   };
+  // 모달 메시지 상태 초기화 (4초 후 자동 사라짐)
+  useEffect(() => {
+    if (modalMessage.message) {
+      const timer = setTimeout(() => {
+        setModalMessage({ type: '', message: '' });
+      }, 4000);
+      return () => clearTimeout(timer);
+    }
+  }, [modalMessage]);
 
   return (
     <div className="login-page-container">
@@ -76,9 +90,13 @@ const LoginPage = () => {
         <button type="submit" className="login-button">
           로그인
         </button>
+        {/* 모달 메시지 내의 메세지가 존재한다면 type으로 클래스 탈부착하여 메세지를 보여준다.*/}
+        {modalMessage.message && (
+          <div className={`modal-message ${modalMessage.type}`}>
+            {modalMessage.message}
+          </div>
+        )}
       </form>
-
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
 
       <p className="sign-up-prompt">
         계정이 없으신가요?{' '}
