2e908133f6a04341d0e433fa9760cd7b4fafc91d
최원빈 <wonbin109@naver.com>
2025-01-30 23:58:04 +0900

    <Feat> Logo 추가 및 그래프 스타일 통일로 성능개선

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-
+import outTeamLogo from '../IMG/walletGuardiansLogoReal.png'; //
 import {
   FaHome,
   FaUser,
@@ -7,7 +7,7 @@ import {
   FaChartPie,
   FaSignOutAlt,
   FaFileImage,
-} from 'react-icons/fa'; // 아이콘 import
+} from 'react-icons/fa';
 import { SidebarContext } from '../context/SidebarContext';
 import '../style/Navbar.scss';
 import { useContext } from 'react';
@@ -15,40 +15,54 @@ import { useContext } from 'react';
 const Navbar = () => {
   const { isSidebarOpen } = useContext(SidebarContext);
   const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       {/* Sidebar */}
       <nav className={`sidebar ${isSidebarOpen ? 'open' : 'closed'}`}>
-        <div className="nav-logo" onClick={() => navigate('/')}>
-          BudgetGuardians
+        <div className="nav-logo" onClick={() => navigate('/main')}>
+          <span className="logo-text">
+            {' '}
+            <img
+              src={outTeamLogo}
+              style={{ marginBottom: '10px', height: '130px' }}
+              alt="Wallet Guardians Logo"
+              className="logo-img"
+            />
+            WalletGuardians
+          </span>
         </div>
         <ul className="nav-menu">
           <li onClick={() => navigate('/main')}>
-            <FaHome style={{ marginRight: '10px' }} />
+            <FaHome className="nav-icon" />
             Home
           </li>
           <li onClick={() => navigate('/profile')}>
-            <FaUser style={{ marginRight: '10px' }} />
+            <FaUser className="nav-icon" />
             내정보
           </li>
           <li onClick={() => navigate('/income')}>
-            <FaMoneyBill style={{ marginRight: '10px' }} />
+            <FaMoneyBill className="nav-icon" />
             수입내역
           </li>
           <li onClick={() => navigate('/expenses')}>
-            <FaMoneyBill style={{ marginRight: '10px' }} />
+            <FaMoneyBill className="nav-icon" />
             지출내역
           </li>
           <li onClick={() => navigate('/graph')}>
-            <FaChartPie style={{ marginRight: '10px' }} />
+            <FaChartPie className="nav-icon" />
             용도별 그래프
           </li>
           <li onClick={() => navigate('/receipt-picture')}>
-            <FaFileImage style={{ marginRight: '10px' }} />
+            <FaFileImage className="nav-icon" />
             영수증 사진 모음
           </li>
           <li onClick={() => navigate('/')}>
-            <FaSignOutAlt style={{ marginRight: '10px' }} />
+            <FaSignOutAlt className="nav-icon" />
             로그아웃
           </li>
         </ul>
