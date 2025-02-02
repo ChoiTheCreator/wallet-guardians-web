6760a2a2e39bd8931d347c411afdbc41f546b0bb
최원빈 <wonbin109@naver.com>
2025-02-02 19:03:11 +0900

    <Feat> vercel.json destination 속성 추가

--- src/api/authApi.jsx
+++ src/api/authApi.jsx
@@ -33,37 +33,3 @@ export const login = async (email, password) => {
     throw error;
   }
 };
-
-// 로그인, 사인업 함수 제외한 모든 API function에 헤더 추가
-export const fetchData = async (endpoint) => {
-  try {
-    const response = await apiClient.get(endpoint, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-        'Custom-Header': 'YourCustomHeaderValue',
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error(
-      '데이터 가져오기 실패:',
-      error.response?.data || error.message
-    );
-    throw error;
-  }
-};
-
-export const postData = async (endpoint, data) => {
-  try {
-    const response = await apiClient.post(endpoint, data, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-        'Custom-Header': 'YourCustomHeaderValue',
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error('데이터 전송 실패:', error.response?.data || error.message);
-    throw error;
-  }
-};
